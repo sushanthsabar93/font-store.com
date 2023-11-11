0c0a852844e36482357596908d815feae04f797a
sushanthsabar93 <149387358+sushanthsabar93@users.noreply.github.com>
2023-11-11 13:24:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+  
+  
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+    
+    
+  
+
+
+
+
+
+
+
+
